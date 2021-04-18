d6070b24d6a4c6febd1567048732908ddf1dd2ef
Rıfat Can Gürbüz <71036790+rcgurbuz@users.noreply.github.com>
2021-04-18 13:04:12 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.use("/posts", postRoutes);
 
 const CONNECTION_URL =
-  "mongodb+srv://javascriptmastery:javascriptmastery123@cluster0.d8cdr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+  "mongodb+srv://YOUR_ID:YOUR_PASSWORD@cluster0.d8cdr.mongodb.net/YOUR_DB_NAME?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
 
 mongoose
